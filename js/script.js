2616cdc78d344ce180d305a244cd8f56253ace6a
Tomasz Zebrun <tomasz.zebrun@tron.net.pl>
2022-02-09 06:39:52 +0100

    6.4 Add dynamic links from JS

--- js/script.js
+++ js/script.js
@@ -1,38 +1,73 @@
-const titleClickHandler = function (){
-    event.preventDefault();
-    const clickedElement = this;
-    //console.log('Link was ciked!');
+'use strict';
 
-    /* remove class 'active' from all article links  */
-    const activeLinks = document.querySelectorAll('.titles a.active');
-    for(let activeLink of activeLinks){
-        activeLink.classList.remove('active');
+{
+    const titleClickHandler = function (){
+        event.preventDefault();
+        const clickedElement = this;
+        //console.log('Link was ciked!');
+
+        /* remove class 'active' from all article links  */
+        const activeLinks = document.querySelectorAll('.titles a.active');
+        for(let activeLink of activeLinks){
+            activeLink.classList.remove('active');
+        }
+
+        /* add class 'active' to the clicked link */
+        //console.log('clickedElement:', clickedElement);
+        clickedElement.classList.add('active');
+
+        /* remove class 'active' from all articles */
+        const activeArticles = document.querySelectorAll('.posts .post.active');
+        for(let activeArticle of activeArticles){
+            activeArticle.classList.remove('active');
+        }
+
+        /* get 'href' attribute from the clicked link */
+        const articleSelector = clickedElement.getAttribute("href");
+        //console.log(articleSelector);
+
+        /* find the correct article using the selector (value of 'href' attribute) */
+        const targetArticle = document.querySelector(articleSelector);
+        //console.log(targetArticle);
+
+        /* add class 'active' to the correct article */
+        targetArticle.classList.add('active');
     }
 
-    /* add class 'active' to the clicked link */
-    //console.log('clickedElement:', clickedElement);
-    clickedElement.classList.add('active');
+    const optArticleSelector = '.post',
+        optTitleSelector = '.post-title',
+        optTitleListSelector = '.titles';
 
-    /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.posts article.active');
-    for(let activeArticle of activeArticles){
-        activeArticle.classList.remove('active');
+    function generateTitleLinks(){
+
+        /* remove contents of titleList */
+        const titleList = document.querySelector(optTitleListSelector);
+        titleList.innerHTML = '';
+
+        /* for each article */
+        const articles = document.querySelectorAll(optArticleSelector);
+        let html = '';
+        for(let article of articles){
+            /* get the article id */
+            const articleId = article.getAttribute("id");
+            /* find the title element */
+            /* get the title from the title element */
+            const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+            /* create HTML of the link */
+            const linkHTML = '<li><a href="#' + articleId +'"><span>' + articleTitle + '</span></a></li>';
+            //console.log(linkHTML);
+            /* insert link into titleList */
+            html = html + linkHTML;
+
+        }
+        titleList.innerHTML = html;
+
+        const links = document.querySelectorAll('.titles a');
+
+        for(let link of links){
+            link.addEventListener('click', titleClickHandler);
+        }
     }
 
-    /* get 'href' attribute from the clicked link */
-    const articleSelector = clickedElement.getAttribute('href');
-    //console.log(articleSelector);
-
-    /* find the correct article using the selector (value of 'href' attribute) */
-    const targetArticle = document.querySelector(articleSelector);
-    //console.log(targetArticle);
-
-    /* add class 'active' to the correct article */
-    targetArticle.classList.add('active');
-}
-
-const links = document.querySelectorAll('.titles a');
-
-for(let link of links){
-    link.addEventListener('click', titleClickHandler);
+    generateTitleLinks();
 }
