03d209a80060a58d1abce60a14d3e0b26a6d4958
Tomasz Zebrun <tomasz.zebrun@tron.net.pl>
2022-02-09 05:02:27 +0100

    6.3 Set clickedElement active

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const titleClickHandler = function (){
-    console.log('Link was ciked!');
-    console.log(event);
+    const clickedElement = this;
+    //console.log('Link was ciked!');
 
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -9,6 +9,8 @@ const titleClickHandler = function (){
     }
 
     /* add class 'active' to the clicked link */
+    //console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
 
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
